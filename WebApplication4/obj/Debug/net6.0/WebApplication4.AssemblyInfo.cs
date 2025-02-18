1b599f68df7a1340a59a0ad3fecdb9a37418bf6b
solimananes386 <solimananes386@gmail.com>
2025-02-18 21:51:09 +0200

    إصلاح مشكلة الحزم وإعادة النشر

--- WebApplication4/obj/Debug/net6.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net6.0/WebApplication4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f70319595fea4687431b9efea1bf61453322cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
